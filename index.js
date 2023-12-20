9622741c703579e6ba50ba4060e7d3312ca089ab
thanh <miktenthanh@gmail.com>
2023-12-20 16:19:46 +0700

    done js

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+(function () {
+  const customSelect = document.querySelector(".custom-select");
+  const selectBtn = document.querySelector(".select-button");
+
+  const selectedValue = document.querySelector(".selected-value");
+  const optionsList = document.querySelectorAll(".select-dropdown li");
+
+  // add click event to select button
+  selectBtn.addEventListener("click", () => {
+    // add/remove active class on the container element
+    customSelect.classList.toggle("active");
+    // update the aria-expanded attribute based on the current state
+    selectBtn.setAttribute(
+      "aria-expanded",
+      selectBtn.getAttribute("aria-expanded") === "true" ? "false" : "true"
+    );
+  });
+
+  optionsList.forEach((option) => {
+    function handler(e) {
+      // Click Events
+      if (e.type === "click" && e.clientX !== 0 && e.clientY !== 0) {
+        selectedValue.textContent = this.children[1].textContent;
+        customSelect.classList.remove("active");
+      }
+      // Key Events
+      if (e.key === "Enter") {
+        selectedValue.textContent = this.textContent;
+        customSelect.classList.remove("active");
+      }
+    }
+
+    option.addEventListener("keyup", handler);
+    option.addEventListener("click", handler);
+  });
+})();
+
 /////////////////////////////////////////////////////
 const dropbtn = document.querySelector(".dropdown");
 const iconSelect = document.querySelector(".icon-select");
 dropbtn.addEventListener("click", () => {
   const dropdownContent = document.querySelector(".dropdown-content");
   dropdownContent.classList.toggle("active");
-  // iconSelect.classList.toggle("icon-select-active");
+  iconSelect.classList.toggle("icon-select-active");
 });
 ////////////////
 const statusInput = document.querySelector("#status");
@@ -42,11 +79,11 @@ const removeErrPassword = (e) => {
 const formm = document.querySelector("form");
 
 formm.addEventListener("submit", (e) => {
-  const selectopt = document.querySelectorAll("input[name=test]");
+  const selectopt = document.querySelectorAll("input[name=dropdownOption]");
   let valueSelecOption;
   for (let i = 0; i < selectopt.length; i++) {
-    if (selectopt[i].selected) {
-      console.log(selectopt[i].value);
+    if (selectopt[i].checked) {
+      valueSelecOption = selectopt[i].value;
     }
   }
   console.log(valueSelecOption);
@@ -96,7 +133,7 @@ formm.addEventListener("submit", (e) => {
     username: username.value,
     password: password.value,
     radio: valueInputName,
-    dropdownOptipn: valueInputSelected,
+    dropdownOptipn: valueSelecOption,
   };
   console.log(inputValues);
 
